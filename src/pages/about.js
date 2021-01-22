13c35130bbb6e1291edbd836ad8928edc62976b9
DumbassNamedTuna <50528857+DumbassNamedTuna@users.noreply.github.com>
2021-01-22 13:53:18 -0800

    about us added

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,31 @@
+import "./about.css";
 import React from 'react';
 
 const About = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '90vh'
-      }}
-    >
-      <h1>About</h1>
-    </div>
+	<>
+		<div className="page">
+			<div className = "about-us">
+				<div className = "content">
+					<h1>About</h1>
+					<h2>
+					We are a part of the Local Leaders of the 21st Century club of Amador Valley High School, a club that strives to improve the environmental state of our community while teaching students leadership skills that will help them in the future.
+					<br></br>
+					<br></br>
+					The Sustainability Shop seeks to make a change by selling products that promote a more environmentally friendly lifestyle, such as tote bags, reusable straws, and shirts made partially from recycled plastic, while the profits towards the Local Leader club's other projects seeking to better our community.
+					<br></br>
+					<br></br>					
+					You can contact us here: 
+					<br></br>
+					<br></br>
+					Phone Number: (925)-998-0564 
+					<br></br>
+					Email: amadorlocalleaders@gmail.com
+					</h2>
+				</div>
+			</div>
+		</div>
+	</>  
   );
 };
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -143,28 +143,6 @@ const Home = () => {
           </div>
         </div>
       </div>
-
-      <div className="about-us">
-
-        <div className="content">
-          <h1>About Us</h1>
-
-          <h2>
-            We are a part of the Local Leaders of the 21st Century club of Amador Valley High School, a club that strives to improve the environmental state of our community while teaching students leadership skills that will help them in the future.
-
-            The Sustainability Shop seeks to make a change by selling products that promote a more environmentally friendly lifestyle, such as tote bags, reusable straws, and shirts made partially from recycled plastic, while the profits towards the Local Leader club's other projects seeking to better our community.
-            
-            <div>
-              <br></br>
-              You can contact us here:
-              <br></br>
-              Phone Number: (925)-998-0564
-              <br></br>
-              Email: amadorlocalleaders@gmail.com
-            </div>
-          </h2>
-        </div>
-      </div>
     </div>
   );
 };
